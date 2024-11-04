956bd6939f0ebc6f789a445f6ef1a4f079ec3a22
Tyler Bowen <tbowen8615@gmail.com>
2024-11-04 07:09:36 -0600

    Add project files.

---
+++ SolarSystemSimulation/SolarSystemSimulation.cpp
@@ -0,0 +1,12 @@
+﻿// SolarSystemSimulation.cpp : Defines the entry point for the application.
+//
+
+#include "SolarSystemSimulation.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
